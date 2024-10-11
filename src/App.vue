d9f20892c40f1c68fe1a231e7ba25056f92a0cfc
panijoanna <pani.joanna93@gmail.com>
2024-10-11 14:30:49 +0200

    feat: add logic and styles to the TheValidatePesel component

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,11 @@
 <script setup>
 import TheTextFileUploadForm from '../src/components/TheTextFileUploadForm.vue'
+import TheValidatePesel from './components/TheValidatePesel.vue'
 </script>
 
 <template>
   <TheTextFileUploadForm />
+  <TheValidatePesel />
 </template>
 
 <style scoped></style>
