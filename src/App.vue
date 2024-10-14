f520d999414832141003303962391292dcccfffd
panijoanna <pani.joanna93@gmail.com>
2024-10-14 12:26:40 +0200

    wip: add TheUserListViewApiHandler component to the App

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,13 @@
 <script setup>
 import TheTextFileUploadForm from '../src/components/TheTextFileUploadForm.vue'
+import TheUserListViewAPIHandler from './components/TheUserListViewAPIHandler.vue'
 import TheValidatePesel from './components/TheValidatePesel.vue'
 </script>
 
 <template>
   <TheTextFileUploadForm />
   <TheValidatePesel />
+  <TheUserListViewAPIHandler />
 </template>
 
 <style scoped></style>
